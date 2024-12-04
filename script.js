414b7200790045bb796b597fa0ce578bfc0ac18c
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-12-04 22:18:03 +0600

    learning

--- script.js
+++ script.js
@@ -1,69 +1,61 @@
 $(function () {
-    $(document).submit((e) => e.preventDefautl());
-
-    let buttonShowHide = $(".show-btn");
-    let getInfo = $(".get-info");
+    let show_btn = $(".show-btn");
+    let get_info_btn = $(".get-info");
     let textArea = $("[name='comments']");
     let username = $("[name='username']");
-    let availability = $('#isAvailable');
-    buttonShowHide.on('click', function()
-    {
-        let passwordField = $("[name='password']");
-        let passwordFieldAttributeValue = passwordField.attr('type');
+    let availabilityText = $("#isAvailable");
 
-        if(passwordField.val() != "")
-        {
-            if(passwordFieldAttributeValue == "password")
-            {
-                passwordField.attr('type', "text");
+    show_btn.on('click', function () {
+        let passwordField = $("[name='password']");
+        let passwordFieldAttrib = passwordField.attr('type');
+        if (passwordField.val() != '') {
+            if (passwordFieldAttrib == "password") {
+                passwordField.attr('type', 'text');
                 $(this).text("Hide");
             }
-            else
-            {
-                passwordField.attr("type", "password");
+            else {
+                passwordField.attr("type", 'password');
                 $(this).text("Show");
             }
         }
     })
-    getInfo.on('click', function()
-    {
-        $.ajax({
-            url: "server/data.txt",
-            method: "GET",
-            data: {},
-            dataType: "text",
-            success: function (data) {
-                textArea.text(data).fadeIn("slow");
+    get_info_btn.on("click", function () {
+        $.ajax(
+            {
+                url: "server/data.txt",
+                method: "GET",
+                data: {},
+                dataType: "text",
+                success: function (data) {
+                    textArea.text(data).fadeIn("slow");
+                }
             }
-        });
+        );
     })
-    username.keyup(function()
-    {
-        let text = $(this).val();
-        if(text != "")
-        {
-            $.ajax({
+    username.on("keyup", function () {
+        let inputValue = $(this).val();
+        $.ajax(
+            {
                 url: "server/data.json",
                 method: "GET",
                 data: {},
-                dataType: "json",
-                success: function(data)
-                {
-                    let name = data["username"];
-                    if(text == name)
-                    {
-                        availability.text("Valid User").css("color", "green");
+                dataType: "JSON",
+                success: function (data) {
+                    let realName = data["username"];
+                    if (inputValue != "") {
+                        if (realName == inputValue) {
+                            availabilityText.text("Found").css("color", "green").fadeIn("slow");
+                        }
+                        else {
+                            availabilityText.text("Not Found!").css("color", "red").fadeIn("slow");
+                        }
                     }
                     else
                     {
-                        availability.text("Invalid User!").css("color", "red");
+                        availabilityText.text("");
                     }
                 }
-            })
-        }
-        else
-        {
-            availability.text("");
-        }
-    });
+            }
+        );
+    })
 })
