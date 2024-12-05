e0e7888abb57b0716cd83f280225e3f04454daeb
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-12-05 09:39:40 +0600

    working

--- script.js
+++ script.js
@@ -4,6 +4,21 @@ $(function () {
     let textArea = $("[name='comments']");
     let username = $("[name='username']");
     let availabilityText = $("#isAvailable");
+    let load_more = $(".load-more");
+    let table = $(".table tbody");
+
+    //Random JSON DATA Add Into Table
+    table.data("name1", "Rahima");
+    table.data("name2", "Jalil");
+    table.data("name3", "Sokina");
+    table.data("name4", "Jarina");
+    table.data("name5", "Karina");
+    table.data("name6", "Ariya");
+    table.data("name7", "Shahina");
+    table.data("name8", "Mithila");
+    table.data("name9", "Hritika");
+    table.data("name10", "Atika");
+    //--------------------------
 
     show_btn.on('click', function () {
         let passwordField = $("[name='password']");
@@ -50,12 +65,47 @@ $(function () {
                             availabilityText.text("Not Found!").css("color", "red").fadeIn("slow");
                         }
                     }
-                    else
-                    {
+                    else {
                         availabilityText.text("");
                     }
                 }
             }
         );
     })
+    let id1 = 3, id2 = 3, count = 1;
+    load_more.on('click', function () {
+        let age = Math.floor(Math.random() * 40);
+        if (age < 5) age += 10;
+
+        if (count > 10) {
+            let tr1 = `<tr>
+                <td>${id1++}</td>
+                <td>Kaka - ${id2++}</td>
+                <td>${age}</td>
+            </tr>`;
+            let tr2 = `<tr>
+                <td>${id1++}</td>
+                <td>Raka - ${id2++}</td>
+                <td>${age += 10}</td>
+            </tr>`;
+            table.append(tr1, tr2);
+        }
+        else {
+            if (count <= 10) {
+                let tr1 = `<tr>
+                <td>${id1++}</td>
+                <td>${table.data("name" + count)}</td>
+                <td>${age}</td>
+            </tr>`;
+                count++;
+                let tr2 = `<tr>
+                <td>${id1++}</td>
+                <td>${table.data("name" + count)}</td>
+                <td>${age += 10}</td>
+            </tr>`;
+                count++;
+                table.append(tr1, tr2);
+            }
+        }
+    });
 })
