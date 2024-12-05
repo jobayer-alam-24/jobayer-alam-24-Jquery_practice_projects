5a9b9018f634e98e980e5d340f7edf8e05982fff
jobayer-alam-24 <158845902+jobayer-alam-24@users.noreply.github.com>
2024-12-05 18:43:14 +0600

    learning

--- script.js
+++ script.js
@@ -126,4 +126,8 @@ $(function () {
         let tr = $(`#${id}`).parents("tr");
         tr.remove();
     })
+    $(document).on('mouseenter', "li", function()
+    {
+        $(this).toggleClass('bg');
+    })
 })
